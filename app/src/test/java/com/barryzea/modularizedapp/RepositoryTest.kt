e91f88832d3cdfa13c7c49ac8794dd44bee53273
hall9zeha <vmwaretars@gmail.com>
2023-11-15 09:57:13 -0500

    Added fetch all registers and delete register tests

--- app/src/test/java/com/barryzea/modularizedapp/RepositoryTest.kt
+++ app/src/test/java/com/barryzea/modularizedapp/RepositoryTest.kt
@@ -7,11 +7,10 @@ import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltTestApplication
 import kotlinx.coroutines.test.runTest
-import org.junit.Test
-
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
@@ -42,6 +41,16 @@ class RepositoryTest {
         hiltRule.inject()
     }
 
+    @Test
+    fun get_all_registers() = runTest {
+        val newRegister = ImageEntity(1,"","test description")
+
+        repository.saveRegister(newRegister)
+
+        val fetchRegisters = repository.getAllRegisters()
+        assertEquals(newRegister,fetchRegisters.first())
+
+    }
    @Test
    fun save_register()= runTest{
        val newRegister = ImageEntity(1,"","test description")
@@ -57,6 +66,16 @@ class RepositoryTest {
         val rowUpdated = repository.updateRegister(updateRegister)
         assertEquals(rowUpdated,1)
 
+    }
+    @Test
+    fun delete_register() = runTest{
+
+        val otherRegister = ImageEntity(2,"","test for delete description")
+        repository.saveRegister(otherRegister)
+
+        val rowDeleted = repository.deleteRegister(otherRegister.id)
+
+        assertEquals(rowDeleted,1)
     }
 
     @Test
